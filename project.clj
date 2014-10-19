aa0ab4b363d804ee929cc469e48255e54e80e007
arrdem <me@arrdem.com>
2014-10-19 02:08:46 -0500

    Flesh out project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-grim "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojure-grimoire/lein-grim "0.1.0-SNAPSHOT"
+  :description "A Leiningen plugin for generating Grimoire documentation"
+  :url "http://github.com/clojure-grimoire/lein-grim"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)

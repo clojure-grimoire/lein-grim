2a2bb98a799f3a046489c14eac0aace0f7da9432
arrdem <me@arrdem.com>
2014-11-18 23:13:58 -0600

    First cut at an 0.3.0 based on a lein alias

--- src/grimoire/doc.clj
+++ src/grimoire/doc.clj
@@ -157,28 +157,53 @@
   (println "Finished" ns)
   nil)
 
-(defn -main
+(declare -main)
 
-  [groupid artifactid version target files-thing]
+(defn -main
+  "Usage: lein grim src <dst>
+  : lein grim artifact <groupid> <artifactid> <version> <dst>
+
+  In source mode, lein-grim traverses the source paths of the current project,
+  enumerating and documenting all namespaces. This is intended for documenting
+  projects for which you have both source and a lein project.
+
+  In artifact mode, lein-grim traverses an artifact on the classpath enumerating
+  and documenting the namespaces therein. This is intended for documenting
+  projects such as clojure.core which may not exist as a covenient lein project
+  but which do exist as artifacts."
+  
+  [p-groupid p-artifactid p-version p-source-paths ;; provided by lein
+   mode-selector & args ;; user provided
+   ]
   (let [config  {:groupid    groupid
                  :artifactid artifactid
                  :version    version
-                 :datastore  {:docs target}}]
-    (if (= :classpath files-thing)
-      ;; classpath searching case
-      (let [pattern (format ".*?/%s/%s/%s.*"
-                            (string/replace groupid "." "/") artifactid version)
-            pattern (re-pattern pattern)]
-        (doseq [e (cp/classpath)]
-          (when (re-matches pattern (str e))
-            (doseq [ns (tns.f/find-namespaces [e])]
-              (when-not (= ns 'clojure.parallel) ;; FIXME: get out nobody likes you
-                (require ns)
-                (write-docs-for-ns config ns))))))
+                 :datastore  {:docs (last args)}}]
+    (case mode-selector
+      ("artifact" :artifact)
+      ,,(let [[groupid artifactid version _dst] args
+              _ (assert groupid)
+              _ (assert artifactid)
+              _ (assert version)
+              _ (assert _dst)
+              pattern (format ".*?/%s/%s/%s.*"
+                              (string/replace groupid "." "/")
+                              artifactid
+                              version)
+              pattern (re-pattern pattern)]
+          (doseq [e (cp/classpath)]
+            (when (re-matches pattern (str e))
+              (doseq [ns (tns.f/find-namespaces [e])]
+                (when-not (= ns 'clojure.parallel) ;; FIXME: get out nobody likes you
+                  (require ns)
+                  (write-docs-for-ns config ns))))))
 
-      ;; source dirs vector case
-      (doseq [ns (->> files-thing
-                      (map io/file)
-                      (tns.f/find-namespaces))]
-        (require ns)
-        (write-docs-for-ns config ns)))))
+      ("src" :src "source" :source)
+      ,,(doseq [ns (->> p-source-paths
+                        (string/split #" ")
+                        (map io/file)
+                        (tns.f/find-namespaces))]
+          (require ns)
+          (write-docs-for-ns config ns))
+
+      (:doc (meta #'-main)))))

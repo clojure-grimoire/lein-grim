30db518c52cdaf8ece2e3d78cec08054437df869
arrdem <me@arrdem.com>
2014-11-27 01:00:37 -0600

    Require implementing namespace

--- src/grimoire/doc.clj
+++ src/grimoire/doc.clj
@@ -6,6 +6,8 @@
             [clojure.tools.namespace.find :as tns.f]
             [clojure.java.classpath :as cp]
             [grimoire.api :as api]
+            [grimoire.api.fs]
+            [grimoire.api.fs.write]
             [grimoire.things :as t]
             [grimoire.util :as util]
             [detritus.var :refer [var->ns var->sym macro?]]

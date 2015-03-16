e6118b322b49d22f707fa6d1930c3e2fe15113fb
arrdem <me@arrdem.com>
2015-03-16 18:54:59 -0500

    Bump deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                 [org.clojure-grimoire/lib-grimoire "0.8.0-SNAPSHOT"]
-                 [me.arrdem/detritus "0.2.2-SNAPSHOT"]
+                 [org.clojure-grimoire/lib-grimoire "0.8.1"]
+                 [me.arrdem/detritus "0.2.2"]
                  [org.clojure/tools.namespace "0.2.7"]]
   :aliases {"grim" ["run" "-m" "grimoire.doc"
                     ,,:project/groupid

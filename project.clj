39d2b489f3ea40624f0de14bb73e105826637999
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-02-21 22:32:38 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://github.com/clojure-grimoire/lein-grim"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure-grimoire/lib-grimoire "0.10.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure-grimoire/lib-grimoire "0.10.4"]
                  [org.clojure/java.classpath "0.2.2"]
                  [org.clojure/tools.namespace "0.2.7"]
-                 [me.arrdem/detritus "0.2.2"]]
+                 [me.arrdem/detritus "0.3.0"]]
   :aliases {"grim" ["run" "-m" "grimoire.doc"
                     ,,:project/group
                     ,,:project/name
